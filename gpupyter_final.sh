b4be28c173dc0f03f6721e047316bcee5cea8d4c
Uriel Cohen <uri-cohen@outlook.com>
2023-11-29 18:36:23 +0100

    Updated gpupyter

--- gpupyter_final.sh
+++ gpupyter_final.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH --time=12:00:00
-#SBATCH -p gpu
-#SBATCH --gpus-per-node=1
 
-#     ^^^ REPLACE WITH PARTITION WITH IDLE NODES! (use gpu_titanrtx_shared (1gpu) or gpu_titanrtx_short (short time))
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+#SBATCH --job-name=CheckEnvironment
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=18
+#SBATCH --time=00:05:00
+#SBATCH --output=slurm_output_%A.out
 
-
-# Make sure the jupyter command is available, either by loading the appropriate modules, sourcing your own virtual environment, etc.
-#module load 2021
-#module load IPython/7.25.0-GCCcore-10.3.0# Choose random port and print instructions to connect
+module purge
+module load 2022
+module load Anaconda3/2022.05
 
 PORT=`shuf -i 5000-5999 -n 1`
 LOGIN_HOST=int4-pub.snellius.surf.nl
